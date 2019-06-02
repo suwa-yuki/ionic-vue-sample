7c5a1877edb939fedb431b184fe20cbefcfec1f9
suwa.yuki <suwa.yuki@classmethod.jp>
2019-06-03 04:21:34 +0900

    [add] First commit.

---
+++ src/views/About.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="about">
+    <ion-header>
+      <ion-toolbar>
+        <ion-buttons slot="start">
+          <ion-back-button></ion-back-button>
+        </ion-buttons>
+        <ion-title>About Page</ion-title>
+      </ion-toolbar>
+    </ion-header>
+    <ion-content class="ion-padding">
+      <h1>This is an about page</h1>
+    </ion-content>
+  </div>
+</template>
---
+++ src/views/Home.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="ion-page">
+    <ion-header>
+      <ion-toolbar>
+        <ion-title>Hello World</ion-title>
+      </ion-toolbar>
+    </ion-header>
+    <ion-content class="ion-padding">
+      <h1>Welcome To @ionic/vue</h1>
+      <img alt="Vue logo" src="../assets/logo.png" />
+      <ion-button @click="onClick()" full>Go to About Page</ion-button>
+    </ion-content>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "home",
+  methods: {
+    onClick () {
+      this.$router.push('about')
+    }
+  }
+}
+</script>
